b1a4c2d1602381b7746feb6dc1a13816324b149f
John Jenkins <jjenksy@gmail.com>
2016-11-09 11:07:16 -0600

    init

---
+++ Gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jjenkins on 11/9/2016.
+ */
+var gulp = require('gulp');
+
+// setup gulp task
+gulp.task('hello-world', function () {
+   console.log("Our first hello world gulp task!!");
+});
